4a7cd80a1463728bedd49a2907c54e6caae0b014
瑀行 <dingguangren@126.com>
2022-01-05 12:48:31 +0800

    feat: java线程阻塞/唤醒机制：Object#wait & Object#notifyAll

--- trainee-biz/trainee-example/src/test/java/com/yuxing/trainee/example/lock/ThreadBlockingAndWakeupTest.java
+++ trainee-biz/trainee-example/src/test/java/com/yuxing/trainee/example/lock/ThreadBlockingAndWakeupTest.java
@@ -3,7 +3,7 @@ package com.yuxing.trainee.example.lock;
 import lombok.Getter;
 import org.junit.Test;
 
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -15,9 +15,9 @@ import java.util.concurrent.BlockingQueue;
  */
 public class ThreadBlockingAndWakeupTest {
 
-
     /**
      * Object: wait、notify、notifyAll
+     * wait、notify、notifyAll 方法是java底层级别提供的与对象监视器配合完成线程间等待/通知机制
      */
     @Test
     public void waitAndNotifyAllTest() throws Exception {
@@ -33,6 +33,75 @@ public class ThreadBlockingAndWakeupTest {
         consumer.start();
 
         Thread.sleep(Integer.MAX_VALUE);
+    }
+
+    /**
+     * Object: wait、notify、notifyAll
+     */
+    @Test
+    public void objectWaitAndNotifyTest() throws Exception {
+        TaskQueue taskQueue = new TaskQueue();
+        List<Thread> taskThreads = new ArrayList<>(5);
+        for (int i = 0; i < 5; i++) {
+            Thread thread = new Thread(() -> {
+                while (true) {
+                    try {
+                        String task = taskQueue.getTask();
+                        System.err.println("execute task: " + task);
+                    } catch (Exception e) {
+                        return;
+                    }
+                }
+            });
+            thread.start();
+            taskThreads.add(thread);
+        }
+
+        Thread addTaskThread = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                String task = "task-" + Math.random();
+                System.err.println("add task: " + task);
+                taskQueue.addTask(task);
+
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        addTaskThread.start();
+        // 等待 addTaskThread 线程执行完毕，再继续执行main Thread后续代码
+        addTaskThread.join();
+
+        Thread.sleep(100);
+
+        for (Thread t : taskThreads) {
+            t.interrupt();
+        }
+    }
+
+
+    static class TaskQueue {
+
+        private Queue<String> taskQueue = new LinkedList<>();
+
+        public synchronized void addTask(String task) {
+            taskQueue.add(task);
+            // 任务队列中添加任务成功时，唤醒获取任务的全部挂起线程
+            this.notifyAll();
+        }
+
+        public synchronized String getTask() throws Exception {
+            while (this.taskQueue.isEmpty()) {
+                // 若当前任务队列为空，则当前获取任务线程挂起等待
+                // wait() 方法必须在当前获取的锁对象上调用
+                // 在线程 wait() 等待时，会释放 this锁
+                this.wait();
+            }
+            return taskQueue.remove();
+        }
+
     }
 
     static class Producer extends Thread {
@@ -149,3 +218,4 @@ public class ThreadBlockingAndWakeupTest {
         }
     }
 }
+
