bc415a8a5b8f3283367e7d4065b19799fed6ab5a
瑀行 <dingguangren@126.com>
2022-01-13 00:09:23 +0800

    docs: 完善CustomThreadPoolExecutor注释

--- trainee-common/src/main/java/com/yuxing/trainee/common/core/concurrent/CustomThreadPoolExecutor.java
+++ trainee-common/src/main/java/com/yuxing/trainee/common/core/concurrent/CustomThreadPoolExecutor.java
@@ -12,30 +12,77 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * 自定义线程池
+ * <b>为什么需要线程池？</b><br/>
+ * Java 语言中，java线程和操作系统线程是一一对应的，这种做法本质上是将java线程的调度权委托给操作系统，
+ * 而操作系统在这方面非常成熟，所以这种做法的好处就是稳定、可靠，但也同样继承了操作系统线程的缺点：创建成本高。 <br/>
+ * 线程池是实现线程复用的一种有效方式。<br/>
+ * <br/>
  *
- *  {@link Executor} 实现了任务发布于任务执行的解耦，任务发布者只需关心业务逻辑的实现，隐藏了任务的执行与线程管理的细节
+ * <b>自定义线程池{@link CustomThreadPoolExecutor}实现说明：</b><br/>
+ * ① 该线程池实现了{@link Executor}接口，其本质是实现了任务发布与任务执行的解耦，任务发布者只需关心业务逻
+ * 辑的实现，并通过{@link Executor#execute(Runnable)}完成任务发布，隐藏了任务的执行与线程管理的细节。<br/>
+ * <br/>
  *
- *  线程池中各参数的作用：
- *  {@link CustomThreadPoolExecutor#corePoolSize} 线程是系统稀缺资源，不能无限制申请，通过corePoolSize 控制线程池中线程的数量
- *  {@link CustomThreadPoolExecutor#taskQueue} 当核心线程已全部创建，仍无法及时处理用户提交的任务，需将其临时存放至任务队列，以便后续线程空闲时处理
- *  {@link CustomThreadPoolExecutor#maxPoolSize} 当任务队列空间耗尽，仍存在用户提交的任务无法处理时，线程池需提供一定的弹性空间允许在大量任务积压时，增加线程数量来处理任务，但仍需限制新增线程数量
- *  {@link CustomThreadPoolExecutor#abortPolicy} 任务放弃策略用于在线程资源已耗尽仍存在无法及时的任务时，提供一种兜底策略来处理这部分任务
- *  {@link CustomThreadPoolExecutor#keepAliveTime} 当任务处理完毕后，一定时间内仍未有新任务时，需及时释放掉空闲的资源，keepLiveTime用于控制空闲资源的存活时间
+ * ② 线程池中各参数的作用：<br/>
+ * {@link CustomThreadPoolExecutor#maxPoolSize}: 1.线程是有限资源，我们不能无限制的创建线程，因此在创
+ * 建线程池时我们需要为其设定一个最大数量，以避免在使用过程中资源耗尽；2.当任务队列空间耗尽，仍存在用户提交的任
+ * 务无法处理时(任务积压)，线程池需提供一定的弹性空间允许在大量任务积压时，增加线程数量来处理任务，但仍需限制新
+ * 增线程数量,当期达到上限就不再创建。 <br/>
+ * {@link CustomThreadPoolExecutor#corePoolSize}: 线程在运行过程中可能只是偶发性地大批量提交任务，而大
+ * 部分时间只是比较零散、少量的任务提交，这就可能导致线程池中线程在完成任务后一段时间内处于空闲状态。如果线程池
+ * 中线程创建后一直存活会导致资源浪费。因此，当线程空闲超过一定时间({@link CustomThreadPoolExecutor#keepAliveTime})
+ * 后应及时将其销毁以回收系统资源；另外，为避免频繁地创建和销毁线程，线程池中也需要缓存一定数量的线程即使处于空闲
+ * 状态也不回收，这类线程我们就称之为核心线程，相应的线程数量就称之为核心线程池大小(corePoolSize)，超出核心线程
+ * 数量之外被创建的线程被称为非核心线程。<br/>
+ * {@link CustomThreadPoolExecutor#taskQueue} 当核心线程已全部创建，仍无法及时处理用户提交的任务，需将
+ * 其临时存放至任务队列，以便后续线程空闲时处理 <br/>
+ * {@link CustomThreadPoolExecutor#abortPolicy} 任务放弃策略用于在线程资源已耗尽仍存在无法及时处理的任务时，
+ * 提供一种兜底策略来处理这部分任务 <br/>
+ * {@link CustomThreadPoolExecutor#keepAliveTime} 当任务处理完毕后，一定时间内仍未有新任务时，需及时释
+ * 放掉空闲的资源，keepLiveTime用于控制空闲资源的存活时间 <br/>
+ * <br/>
  *
- *  线程池工作流程：
- *  1. 任务提交时，优先创建核心线程来处理任务
- *  2. 核心线程资源耗尽，则将任务加入阻塞队列，等待空闲核心线程处理
- *  3. 若阻塞队列资源耗尽，仍存在任务无法及时处理，则尝试创建非核心线程进行处理
- *  4. 若非核心线程资源耗尽，剩余任务通过任务放弃策略进行处理
- *  5. 当任务处理完毕后，一段时间后无法获取任务，则将闲置资源释放
- *  6. 对于 1&3 中创建的线程，当线程创建后即加入线程池中，并开启线程轮询获取任务，并进行执行，在无法获取任务后等待资源释放
+ * ③ 线程池工作流程：<br/>
+ *  1. 任务提交时，优先创建核心线程来处理任务 <br/>
+ *  2. 核心线程资源耗尽，则将任务加入阻塞队列，等待空闲核心线程处理 <br/>
+ *  3. 若阻塞队列资源耗尽，仍存在任务无法及时处理，则尝试创建非核心线程进行处理 <br/>
+ *  4. 若非核心线程资源耗尽，剩余任务通过任务放弃策略进行处理 <br/>
+ *  5. 当任务处理完毕后，一段时间后无法获取任务，则将闲置资源(非核心线程)释放 <br/>
+ *  6. 对于 1&3 中创建的线程，当线程创建后即加入线程池中，并开启线程轮询获取任务，并进行执行，在无法获取任务后
+ *  等待资源释放 <br/>
+ * <br/>
  *
- *  // TODO(yuxing): 缺少线程池生命周期管理
+ * ④ 采用生产者-消费者模型{@link com.yuxing.trainee.example.concurrent.designpattern.producerconsumer}
+ * 完成任务的提交 & 任务的消费 <br/>
+ *  - {@link CustomThreadPoolExecutor#execute(Runnable)} 完成生产者角色 <br/>
+ *  - {@link Worker} 完成消费者角色 <br/>
+ * <br/>
  *
- *  采用生产者-消费者模型完成任务的提交 & 任务的消费
- *  - {@link CustomThreadPoolExecutor#execute(Runnable)} 完成生产者角色
- *  - {@link Worker} 完成消费者角色
+ * ⑤ 采用Worker Thread模式{@link com.yuxing.trainee.example.concurrent.designpattern.workerthread} <br/>
+ * <br/>
+ *
+ * <b>如何实现线程复用？</b><br/>
+ * 线程在执行完 {@link Thread#run()}方法后就会被回收，因此要想实现线程复用，就应避免退出run()方法。这里，我们可以
+ * 通过<b>循环向任务队列获取任务</b>的方式来实现；另一方面，若任务队列为空时，无法获取任务仍可能导致线程退出run()方法，
+ * 这种情况下，我们可以通过阻塞线程的方式，等待外部提交任务。通过<b>循环获取+阻塞等待</b>的方式，就可以实现线程复用的
+ * 目的。<br/>
+ * <br/>
+ * <b>如何尽量实现线程复用？(为什么在线程池工作流程中在核心线程资源耗尽后不继续创建线程而是优先放入任务队列，待队列满了
+ * 才继续创建非核心线程？)</b><br/>
+ * 线程池中需要使用到任务队列进行缓存不能及时处理的任务，那么任务队列的使用时机就可以有一下两种：<br/>
+ * 1.当线程数已达到maximumPoolSize上限要求，并且所有线程都处于工作状态时，此后外部提交的任务才被缓存到任务队列中<br/>
+ * 2.当核心线程corePoolSize达到上限要求时，此后外部提交的任务就被缓存到任务队列中，当任务队列满了之后，才通过创建
+ *   非核心线程来处理任务<br/>
+ * 显然第二种方案更优，此方案一方面尽可能的实现了资源(核心线程)的最大化复用，另一方面可以避免创建非核心线程(少占用系
+ * 统稀缺资源); 并且该方案在应对“偶发性地大批量提交任务，大部分时间只是比较零散地少量任务提交”这种情况较为合适。但该方案
+ * 在一定程度上降低了任务的处理速度。<br/>
+ * <br/>
+ *
+ * TODO(yuxing):
+ *   ① 缺少线程池生命周期管理
+ *   ② 采用Two-Phase Termination 模式{@link com.yuxing.trainee.example.concurrent.designpattern.twophasetermination} 关闭线程
+ *   ③ 扩展 {@link com.yuxing.trainee.common.core.concurrent.CustomThreadPoolExecutor} 使其支持执行任务并获取返回值
+ *   ④ 任务放弃策略处理
  *
  *
  * @author yuxing
