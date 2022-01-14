66653f5327a6454f9a28d689b355eed2c2d4b7f0
瑀行 <dingguangren@126.com>
2022-01-14 18:08:50 +0800

    feat: 移除detection模块

--- trainee-biz/trainee-detection/src/main/java/com/yuxing/trainee/detection/domain/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * @author yuxing
- * @since 2022/1/12
- */
-package com.yuxing.trainee.detection.domain;
--- trainee-biz/trainee-detection/src/main/java/com/yuxing/trainee/detection/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * 户型检测实现方案验证
- *
- * @author yuxing
- * @since 2022/1/12
- */
-package com.yuxing.trainee.detection;
