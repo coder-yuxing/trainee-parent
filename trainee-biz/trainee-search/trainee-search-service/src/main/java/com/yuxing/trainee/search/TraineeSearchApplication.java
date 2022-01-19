97c252523e7bcf903c1889f4ac31f704daf81d65
瑀行 <dingguangren@126.com>
2022-01-19 14:04:28 +0800

    chore: 移除无用依赖

--- trainee-biz/trainee-search/trainee-search-service/src/main/java/com/yuxing/trainee/search/TraineeSearchApplication.java
+++ trainee-biz/trainee-search/trainee-search-service/src/main/java/com/yuxing/trainee/search/TraineeSearchApplication.java
@@ -1,6 +1,5 @@
 package com.yuxing.trainee.search;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -13,7 +12,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
-@MapperScan("com.yuxing.trainee.search.infrastructure.dao")
 public class TraineeSearchApplication {
 
     public static void main(String[] args) {
